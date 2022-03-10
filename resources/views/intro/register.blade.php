a8629465c42041c5b153012b7292eafcc7b2d501
gosh19 <matiascianci@gmail.com>
2022-03-10 11:23:23 -0300

    quitadod e boton en registro

--- resources/views/intro/register.blade.php
+++ resources/views/intro/register.blade.php
@@ -5,7 +5,7 @@
 
 
         {{-- navBar --}}
-        <div class="grid grid-cols-8 p-3 items-center gap-7 md:gap-0">
+        <div class="grid grid-cols-8 p-3 items-center justify-end gap-7 md:gap-0">
             <div class="col-start-1 col-span-8 md:col-span-1 justify-self-center">
                 <a href="/">
 
@@ -13,7 +13,7 @@
                 </a>
             </div>
 
-            <div class="md:col-start-5 col-span-2 md:col-span-1 justify-self-center">
+            <div class="md:col-start-6 col-span-2 md:col-span-1 justify-self-center">
                 <button
                     class="text-white hover:text-blueGray-200 p-3 transform hover:scale-105 transition duration-500 focus:outline-none">
                     Cursos
@@ -21,12 +21,6 @@
             </div>
             <div class="col-span-2 md:col-span-1 justify-self-center">
                 @livewire('nav-bar.carrito')
-            </div>
-            <div class="col-start-7 col-span-1 justify-self-center">
-                <button
-                    class="p-3 w-full text-white bg-indigo-600 hover:bg-indigo-700 rounded-xl shadow-sm font-bold transform hover:scale-105 transition duration-500 focus:outline-none">
-                    Ingresar al aula
-                </button>
             </div>
         </div>
 
