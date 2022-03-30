d6c43f3df0ac81bcc24d7cfc7101a72e9e8d0ab7
gosh19 <matiascianci@gmail.com>
2022-03-30 16:05:12 -0300

    testmp

--- resources/js/components/Vue/Register/Step3.vue
+++ resources/js/components/Vue/Register/Step3.vue
@@ -1,42 +1,15 @@
+
+<template>
+  <div class="w-full bg-white">
+3
+  </div>
+</template>
+
 <script>
 export default {
   setup() {},
 };
-</script>
 
-<template>
-  <div class="w-full bg-white">
-    <form id="form-checkout">
-      <input type="text" name="cardNumber" id="form-checkout__cardNumber" />
-      <input
-        type="text"
-        name="cardExpirationDate"
-        id="form-checkout__cardExpirationDate"
-      />
-      <input
-        type="text"
-        name="cardholderName"
-        id="form-checkout__cardholderName"
-      />
-      <input
-        type="email"
-        name="cardholderEmail"
-        id="form-checkout__cardholderEmail"
-      />
-      <input type="text" name="securityCode" id="form-checkout__securityCode" />
-      <select name="issuer" id="form-checkout__issuer"></select>
-      <select
-        name="identificationType"
-        id="form-checkout__identificationType"
-      ></select>
-      <input
-        type="text"
-        name="identificationNumber"
-        id="form-checkout__identificationNumber"
-      />
-      <select name="installments" id="form-checkout__installments"></select>
-      <button type="submit" id="form-checkout__submit">Pagar</button>
-      <progress value="0" class="progress-bar">Cargando...</progress>
-    </form>
-  </div>
-</template>
+
+
+</script>
